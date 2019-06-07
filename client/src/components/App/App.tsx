479e6029432cf51de8d73e22dd94cfdc0ee32e93
Andre Suzanovich <supius@gmail.com>
2019-06-08 00:12:56 +0200

    add TSX client

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        Over to you...
+    </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import React from 'react';
+
+export default () => (
+  <header>
+    <a href="/">
+      <span>Skyscanner</span>
+      <img alt="Skyscanner" src={logo} />
+    </a>
+    {/* TODO burger menu icon */}
+  </header>
+)
