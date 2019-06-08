7ecd58e1b8454ec7b2404462dc1b18bd4067a4c4
Andre Suzanovich <supius@gmail.com>
2019-06-08 22:20:58 +0200

    add leg formating

--- client/src/components/Leg/Leg.tsx
+++ client/src/components/Leg/Leg.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import LegProps from './LegProps';
+import './Leg.css';
 
 const Leg: React.FC<LegProps> = (props: LegProps) => (
-  <div>{`${props.originStationCode} -> ${props.destinationStationCode}`}</div>
+  <div className="container leg-container">
+    <div className="row">
+      <div className="col-1 leg-col">
+        E
+      </div>
+      <div className="col-2 leg-col">
+        <div>7:00</div>
+        <div className="leg-place-code">{props.originStationCode}</div>
+      </div>
+      <div className="col-1 leg-col leg-arrow">
+        <span>&nbsp;&#10140;&nbsp;</span>
+      </div>
+      <div className="col-2 leg-col">
+        <div>12:00</div>
+        <div className="leg-place-code">{props.destinationStationCode}</div>
+      </div>
+      <div className="col leg-col leg-col-last">
+        <div className="leg-duration">1h 25</div>
+        <div className="leg-direct">Direct</div>
+      </div>
+
+    </div>
+  </div>
 )
 
 export default Leg
