51067ab6b9433c5e01e0900ffefaf09b6fc1c70c
Andre Suzanovich <supius@gmail.com>
2019-06-08 13:13:15 +0200

    add query formating

--- client/src/components/QueryInfo/QueryInfo.tsx
+++ client/src/components/QueryInfo/QueryInfo.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import IQueryInfoProps from './QueryInfoProps';
+import './QueryInfo.css';
 
 const QueryInfo: React.FC<IQueryInfoProps> = (props: IQueryInfoProps) => (
-  <React.Fragment>
-
-
-
-
-
-  </React.Fragment>
+  <div className="query-info">
+    <div className="direction-info">
+      {props.fromCode}
+      <span>&#10140;</span>
+      {props.toCode}
+    </div>
+    <div className="travellers-info">2 travellers, economy</div>
+  </div>
 )
 
 export default QueryInfo
+;
